86b14c677d094eea043bc3e13dbb2dd5b29b1a85
Wipersee <kesterera@gmail.com>
2020-09-03 19:20:07 +0300

    added a few work on frontend

--- core_logic/urls.py
+++ core_logic/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.dashboard, name='dashboard'),
+    path('pie_chart/', views.pie_chart, name='pie_chart'),
+    path('graph_chart/', views.graph_chart, name='graph_chart'),
 ]
--- core_logic/views.py
+++ core_logic/views.py
@@ -4,4 +4,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def dashboard(request):
-    return render(request,'core_logic/main_stat.html')
+    return render(request,'core_logic/main_stat.html', {'active':'dashboard'})
+
+@login_required
+def pie_chart(request):
+    return render(request, 'core_logic/pie.html', {'active': 'pie'})
+
+@login_required
+def graph_chart(request):
+    return render(request, 'core_logic/graph.html', {'active': 'graph'})
