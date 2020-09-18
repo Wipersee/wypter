1890577449caef2e81a092a9fe35ee61b7cc1e5a
Wipersee <kesterera@gmail.com>
2020-09-18 11:19:31 +0300

    fixed bugs

--- wypter/core_logic/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Wallet)
-admin.site.register(models.Category)
-admin.site.register(models.Extend)
-admin.site.register(models.Income)
--- wypter/core_logic/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreLogicConfig(AppConfig):
-    name = 'core_logic'
